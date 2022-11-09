f3ac1f9016634e0bf678b31ba12b5c5895352ed1
BUKASJ <chukwuebukaedej@gmail.com>
2022-11-08 19:30:02 -0800

    returned pointer points to a newly allocated space in memory which contains the contents of s1, followed by the contents of s2, and null terminated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,10 @@
  *@s1: string 1
  *@s2: string 2
  *
- * Return: NULL on failure
+ * Return: return pointer should point to a newly allocated space in memory
+ * which contains the contents of s1, followed by the contents of s2
+ * and null terminated
+ * return NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
@@ -16,8 +19,13 @@ char *str_concat(char *s1, char *s2)
 	char *result;
 
 	len1 = len2 = 0;
-
 	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
+	if (s2 != NULL)
 	{
 		i = 0;
 		while (s2[i++] != '\0')
@@ -27,30 +35,10 @@ char *str_concat(char *s1, char *s2)
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (result == NULL)
 		return (NULL);
-
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 	for (j = 0; j < len2; j++, i++)
 		result[i] = s2[j];
 	result[len] = '\0';
-
 	return (result);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
