65246b89d379ff8dbaeb2f1c3baf6d97b4186aa1
BUKASJ <chukwuebukaedej@gmail.com>
2022-11-29 17:01:37 -0800

    a function that prints a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,80 @@
+#include "lists.h"
+#include <stdio.h>
+
+size_t looped_listint_lens(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
+/**
+ * looped_listint_lens - counts the number of unique nodes
+ * in a looped listint_t linked list
+ * @head: a pointer to the head of the listint_t list
+ *
+ * Return: if the list is not looped - 0
+ * otherwise return the number of unique nodes in the list
+ */
+
+size_t looped_listint_lens(const listint_t *head)
+{
+	const listint_t *tent, *peg;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	tent = head->next;
+	peg = (head->next)->next;
+
+	while (peg)
+	{
+		if (tent == peg)
+		{
+			tent = head;
+			while (tent != peg)
+			{
+				nodes++;
+				tent = tent->next;
+				peg = peg->next;
+			}
+
+				tent = tent->next;
+				while (tent != peg)
+				{
+					nodes++;
+					tent = tent->next;
+				}
+				return (nodes);
+		}
+			tent = tent->next;
+			peg = (peg->next)->next;
+	}
+	return (0);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: a pointer to the address of the head of the listint_t list
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes, index = 0;
+
+	nodes = looped_listint_lens(head);
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+	}
+}
