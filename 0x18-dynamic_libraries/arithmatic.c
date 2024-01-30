aed34ce3518092b441efe6f5c275c5365d1dc9fc
BUKASJ <chukwuebukaedej@gmail.com>
2024-01-29 19:42:24 -0800

    dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/arithmatic.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - add two integer
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: Sum of the numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: difference of the numbers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul  - multiplies two integer
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: Product of the numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div  - divides two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: the numbers
+ */
+ 
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod  - modulus two integer
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: modulus of the numbers
+ */
+ 
+int mod(int a, int b)
+{
+	return (a % b);
+}
