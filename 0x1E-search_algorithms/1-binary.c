6c053d9a72714acd4a335b58051a3364716f8a42
BUKASJ <chukwuebukaedej@gmail.com>
2024-05-24 02:50:58 +0100

    searches for a value in a sorted array of integers

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,44 +1,58 @@
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
- * binary_search - Searches for a value in a sorted array of integers.
- *
- * @array: Pointer to the first element of the array to search.
+ * print_array - Prints the array being searched
+ * @array: The array to print
+ * @left: The starting index
+ * @right: The ending index
+ */
+
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		if (i > left)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - Searches for a value in a sorted array of integers
+ * using the Binary search algorithm.
+ * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
- * @value: The value to search for.
+ * @value: Value to search for.
  *
- * Return: The index of the value in the array, or -1 if not found.
+ * Return: The index where the value is located, or -1 if the value is not
+ * present or the array is NULL.
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0;
-	size_t high = size - 1;
-	size_t i;
-	size_t mid;
+	size_t left, right, mid;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
-	while (low <= high)
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
 	{
-		mid = (low + high) / 2;
-
-		printf("Searching in array:");
-		for (i = low; i <= high; i++)
-		{
-			if (i != low)
-				printf(", ");
-			printf("%d", array[i]);
-		}
-		printf("\n");
-
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] < value)
-			low = mid + 1;
-		else
-			high = mid - 1;
+		print_array(array, left, right);
+	mid = (left + right) / 2;
+	if (array[mid] == value)
+		return (mid);
+	if (array[mid] < value)
+		left = mid + 1;
+	else
+		right = mid - 1;
 	}
 
 	return (-1);
